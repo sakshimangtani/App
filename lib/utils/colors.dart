67ede62dce33c820a3c3fc798533b23b17b6b6c1
Risshab Singla <risshab2singla@gmail.com>
2022-09-17 20:15:40 +0530

    first commit

---
+++ lib/utils/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+// const buttonColor = Color.fromRGBO(145, 71, 255, 1);
+const buttonColor = Colors.black;
+const backgroundColor = Colors.white;
+// const backgroundColor = Colors.grey;
+
+// const secondaryBackgroundColor = Color.fromRGBO(239, 239, 241, 1);
+const secondaryBackgroundColor = Colors.deepPurple;
+
+// const primaryColor = Colors.black;
+const primaryColor = Colors.deepPurple;
